43fc5361c952c23d45d450069b18eb2fb61e91ba
Rahul555 <f20202108@hyderabad.bits-pilani.ac.in>
2023-04-17 14:49:24 +0530

    Login semi complete

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import LoginPagePic from "./LoginPagePic.jsx";
+import Login from "./Login.jsx";
+
+function App() {
+  return <div className="outer">
+  <Login />
+     <LoginPagePic />
+    </div>
+}
+
+export default App;
---
+++ src/Login.jsx
@@ -0,0 +1,38 @@
+import React, {useState} from "react";
+import Logo from "./logo.jsx"
+function Login(){
+  const [isMousedOver, setMouseOver] = useState(false);
+  function handleClick()
+  {
+    setMouseOver(true);
+  }
+  function handleMouseOver() {
+    setMouseOver(true);
+  }
+
+  function handleMouseOut() {
+    setMouseOver(false);
+  }
+    return <div className="column">
+    <Logo /> 
+    <div className="container">
+      <h2>Login</h2>
+      <br></br>
+      
+      <input type="text" placeholder="email-id" />
+      <input type="text" placeholder="password" />
+      <button
+        style={{ backgroundColor: isMousedOver ? "white" : null, color: isMousedOver? "#4c3a8d": "white" }}
+        onClick={handleClick}
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
+      >Login
+      </button>
+    </div>
+    <p>Don't have an account? Sign up here</p>
+    </div>
+    
+    
+}
+
+export default Login;
